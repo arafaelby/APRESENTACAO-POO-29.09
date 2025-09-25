ece9addadd196cdfed84062a727004ccb71f59dc
arafaelby <alexrafaellopesavelino@gmail.com>
2025-09-25 07:30:49 -0300

    1. Variáveis: Uso de variáveis locais e attributos de instância.

---
+++ scr/Variaveis/Bingo.java
@@ -0,0 +1,13 @@
+package Variaveis;
+
+public class Bingo {
+    String nomeBingo;
+
+    Bingo(String nomeBingo){
+       this.nomeBingo = nomeBingo;
+    }
+    public void anunciarVencedor(){
+        String vencedor = "felipe";
+        System.out.println("O vencedor foi: " + vencedor);
+    }
+}
---
+++ scr/Variaveis/MainBingo.java
@@ -0,0 +1,8 @@
+package Variaveis;
+
+public class MainBingo {
+    public static void main (String[] args) {
+        Bingo BingoMaes = new Bingo("Maes");
+        BingoMaes.anunciarVencedor();
+    }
+}
